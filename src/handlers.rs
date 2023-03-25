980d8e4529939b3be2297360a171144886b1078e
Michael Collins <michael.collins@gmail.com>
2023-03-25 11:38:10 -0700

    my first web api router!

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,6 @@
-use hyper::{Body, Method, Request, Response, StatusCode};
+use hyper::{Body, Method, Request, Response, StatusCode, Uri};
 use std::net::SocketAddr;
+use url::Url;
 
 static INDEX: &[u8] =
     b"<html><head><title>kv</title></head><body><h1>kv</h1>A simple Key Value store! <br /><br /> \
@@ -11,19 +12,37 @@ $ curl localhost:3000/kv/foo \
 
 static TODO: &[u8] = b"TODO";
 
+// route_root returns first segment of uri path
+//   for example "/foo/bar" -> "/foo", "/" -> "/", "/blah/blah?blah" -> "/blah" etc
+// TODO tests and error handling (too many unwraps!)
+fn route_root(req_uri: &Uri) -> String {
+    // url crate only parses absolute urls so use example.com as fake base for url
+    let root_url = Url::parse("http://example.com").unwrap();
+    let request_url = root_url.join(req_uri.to_string().as_str()).unwrap();
+    let path_segments: Vec<&str> = request_url.path_segments().unwrap().collect();
+    format!("/{}", path_segments[0])
+}
+
 // base router which calls our other handlers or returns 404
 pub async fn router(req: Request<Body>, addr: SocketAddr) -> Result<Response<Body>, hyper::Error> {
-    println!("request: {} from client: {}", req.uri(), addr);
+    let route = route_root(req.uri());
+    let route = route.as_str();
+    println!(
+        "request: {} from client: {} - route: {}",
+        req.uri(),
+        addr,
+        route
+    );
 
-    match (req.method(), req.uri().path()) {
+    match (req.method(), route) {
         (&Method::GET, "/") | (&Method::GET, "/index.html") => Ok(Response::new(INDEX.into())),
 
         (&Method::GET, "/hello") => hello(req).await,
 
-        // TODO need to match on "/kv/{foo}" not just "/kv" ...
         (&Method::GET, "/kv") | (&Method::POST, "/kv") => Ok(Response::new(TODO.into())),
 
         _ => {
+            println!("unknown route: {}, returning 404 ...", route);
             let mut not_found = Response::default();
             *not_found.status_mut() = StatusCode::NOT_FOUND;
             Ok(not_found)

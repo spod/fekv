489a18fd440cc5b90ded99bfd9c13db6281ecf95
Michael Collins <michael.collins@gmail.com>
2023-04-03 23:17:25 -0700

    add basic parameter and query handling to http router

--- src/handlers.rs
+++ src/handlers.rs
@@ -17,47 +17,72 @@ static TODO: &[u8] = b"TODO";
 
 // route_root returns first segment of uri path
 //   for example "/foo/bar" -> "/foo", "/" -> "/", "/blah/blah?blah" -> "/blah" etc
+// also returns remainder of path and query parameters (if any)
 // TODO tests and error handling (too many unwraps!)
-fn route_root(req_uri: &Uri) -> String {
+fn route_root(req_uri: &Uri) -> (String, Option<String>, Option<String>) {
     // url crate only parses absolute urls so use example.com as fake base for url
     let root_url = Url::parse("http://example.com").unwrap();
     let request_url = root_url.join(req_uri.to_string().as_str()).unwrap();
     let path_segments: Vec<&str> = request_url.path_segments().unwrap().collect();
-    format!("/{}", path_segments[0])
+    let query: Option<String> = request_url.query().map(str::to_string);
+    match path_segments.len() {
+        0 => return (String::from("/"), None, query),
+        1 => return (format!("/{}", path_segments[0]), None, query),
+        _ => {
+            let rest = path_segments.split_at(1).1;
+            return (
+                format!("/{}", path_segments[0]),
+                Some(rest.join("/")),
+                query,
+            );
+        }
+    }
 }
 
 // base router which calls our other handlers or returns 404
 pub async fn router(
     req: Request<Body>,
     addr: SocketAddr,
-    store: Arc<Mutex<MemStore<'_>>>,
+    _store: Arc<Mutex<MemStore<'_>>>,
 ) -> Result<Response<Body>, hyper::Error> {
-    let route = route_root(req.uri());
+    let (route, rest, _query) = route_root(req.uri());
     let route = route.as_str();
-    println!(
-        "request: {} from client: {} - route: {}",
-        req.uri(),
-        addr,
-        route
-    );
+    let rest = rest.unwrap_or(String::from(""));
 
-    println!("store: {:?}", store);
+    if route != "/favicon.ico" {
+        println!(
+            "request: {} from client: {} with route: {}",
+            req.uri(),
+            addr,
+            route
+        );
+    }
+
+    // println!("store: {:?}", store);
     match (req.method(), route) {
         (&Method::GET, "/") | (&Method::GET, "/index.html") => Ok(Response::new(INDEX.into())),
 
-        (&Method::GET, "/hello") => hello(req).await,
+        (&Method::GET, "/hello") => hello(req, rest).await,
 
         (&Method::GET, "/kv") | (&Method::POST, "/kv") => Ok(Response::new(TODO.into())),
 
+        (&Method::GET, "/favicon.ico") => response_404().await,
         _ => {
             println!("unknown route: {}, returning 404 ...", route);
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
+            response_404().await
         }
     }
 }
 
-pub async fn hello(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+pub async fn hello(_req: Request<Body>, name: String) -> Result<Response<Body>, hyper::Error> {
+    if name != "" {
+        return Ok(Response::new(Body::from(format!("Hello {}!", name))));
+    }
     Ok(Response::new(Body::from("Hello World!")))
+}
+
+pub async fn response_404() -> Result<Response<Body>, hyper::Error> {
+    let mut not_found = Response::default();
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
 }

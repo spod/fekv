b6c878cd89dd93caa9b777e42c65334b587cb05f
Michael Collins <michael.collins@gmail.com>
2023-04-12 17:51:35 -0700

    Start to implement db backed raft store
    TODO: Not sure how best to handle raft-rs::Entrys having
    weird protobuf fields which don't serialize easily

--- src/raftstore.rs
+++ src/raftstore.rs
@@ -1,4 +1,3 @@
-
 // https://www.pingcap.com/blog/implement-raft-in-rust/
 // https://github.com/etcd-io/etcd/blob/main/contrib/raftexample/raft.go
 // https://github.com/etcd-io/etcd/blob/main/server/storage/wal/doc.go
@@ -12,7 +11,7 @@
 //
 // How to store this in LMDB?
 //  - One DB for raft log entries - key is term/index, value is log entry
-//  - One DB for Config - key is config item, value is state for config 
+//  - One DB for Config - key is config item, value is state for config
 //  See hashicorp/raft-mdb for this in go
 
 //
@@ -21,45 +20,136 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
+use std::fs::create_dir_all;
+use std::path::Path;
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
-use std::sync::{Arc, RwLock};
-
-use heed::types::{ByteSlice, OwnedType, Str};
+use heed::types::{ByteSlice, OwnedType, SerdeBincode, Str};
 use heed::{Database, Env, EnvOpenOptions};
+use raft::prelude::*;
+use raft::{Error, StorageError};
+use serde::{Deserialize, Serialize};
 
-use raft::{
-    self,
-    eraftpb::{self, ConfState, Entry, HardState, Snapshot},
-    Error as RaftError, GetEntriesContext, RaftState, Ready, Storage, StorageError,
-};
+const DB_ENTRIES: &str = "entries";
 
+const DB_ENV: &str = "raft.mdb";
 const DB_PATH: &str = "./data";
-const DB_NAME: &str = "raft.mdb";
 const DB_STORE_SIZE: usize = 1_073_741_824;
 
-struct RaftDB {
+// Serde compatible clones of raft::Entry/EntryType
+// TODO need to write some helpers to map to/from Entry as can't figure
+// out how to make Serde remote derive work with both raft::Entry special
+// protobuf fields (unknown_fields, cached_size)
+// and heed::Database<...> declaration - where does #[serde(with = "EntryRef")] go?
+
+#[derive(Clone, Serialize, Deserialize)]
+struct EntryRef {
+    pub entry_type: EntryTypeRef,
+    pub term: u64,
+    pub index: u64,
+    pub data: ::bytes::Bytes,
+    pub context: ::bytes::Bytes,
+    pub sync_log: bool,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+enum EntryTypeRef {
+    EntryNormal = 0,
+    EntryConfChange = 1,
+    EntryConfChangeV2 = 2,
+}
+
+impl EntryTypeRef {
+    pub fn to_entry_type(&self) -> EntryType {
+        match self {
+            crate::raftstore::EntryTypeRef::EntryNormal => raft::eraftpb::EntryType::EntryNormal,
+            crate::raftstore::EntryTypeRef::EntryConfChange => raft::eraftpb::EntryType::EntryConfChange,
+            crate::raftstore::EntryTypeRef::EntryConfChangeV2 => raft::eraftpb::EntryType::EntryConfChangeV2,
+        }
+    }
+}
+
+impl EntryRef {
+    pub fn to_entry(&self) -> Entry {
+        Entry {
+            entry_type: self.entry_type.to_entry_type(),
+            term: self.term,
+            index: self.index,
+            data: self.data.to_owned(),
+            context: self.context.to_owned(),
+            sync_log: self.sync_log,
+            unknown_fields: todo!(),
+            cached_size: todo!(),
+        }
+    }
+}
+
+pub struct RaftDB {
     env: Env,
-    // TODO - likely need to use heed Serde functionality, ByteSlice is a placeholder for now
-    conf: Database<Str, ByteSlice>,         
-    logs: Database<OwnedType<i64>, ByteSlice>
+    entries: Database<OwnedType<u64>, SerdeBincode<EntryRef>>,
+    raft_state: RaftState,
+    snapshot_metadata: SnapshotMetadata,
 }
 
 impl RaftDB {
     pub fn new() -> RaftDB {
-        todo!()
+        let db_path = Path::join(Path::new(&DB_PATH), &DB_ENV);
+        _ = create_dir_all(&db_path);
+        let env = EnvOpenOptions::new()
+            .map_size(DB_STORE_SIZE) // 10MB
+            .max_dbs(3)
+            .open(db_path)
+            .unwrap();
+        let entries = env.create_database(Some(&DB_ENTRIES)).unwrap();
+        let raft_state = RaftState::new(HardState::new(), ConfState::new());
+        // TODO write initial raft_state to conf database, for now we use raft::storage::RaftState;
+        RaftDB {
+            env: env,
+            entries: entries,
+            raft_state: raft_state,
+            snapshot_metadata: SnapshotMetadata::new(),
+        }
     }
 
+    fn first_index(&self) -> u64 {
+        let rtxn = self.env.read_txn().unwrap();
+        let r = self.entries.first(&rtxn);
+        if r.is_err() {
+            return self.snapshot_metadata.index + 1;
+        }
+        match r.unwrap() {
+            Some(e) => e.0,
+            None => self.snapshot_metadata.index + 1,
+        }
+    }
+
+    fn last_index(&self) -> u64 {
+        let rtxn = self.env.read_txn().unwrap();
+        let r = self.entries.last(&rtxn);
+        if r.is_err() {
+            return self.snapshot_metadata.index + 1;
+        }
+        match r.unwrap() {
+            Some(e) => e.0,
+            None => self.snapshot_metadata.index + 1,
+        }
+    }
+    fn get_entry(&self, idx: u64) -> Entry {
+        let rtxn = self.env.read_txn().unwrap();
+        let res = self.entries.get(&rtxn, &idx);
+        let er = res.unwrap().unwrap();
+        er.to_entry()
+    }
 }
 
 pub struct RaftDiskStorage {
-    raftdb: Arc<RwLock<RaftDB>>
+    raftdb: Arc<RwLock<RaftDB>>, // Do we need to use tokio::sync::RwLock?
 }
-
 
 impl RaftDiskStorage {
     pub fn new() -> RaftDiskStorage {
-        RaftDiskStorage { 
-            raftdb: Arc::new(RwLock::new(RaftDB::new()))
+        RaftDiskStorage {
+            raftdb: Arc::new(RwLock::new(RaftDB::new())),
         }
     }
 
@@ -71,17 +161,28 @@ impl RaftDiskStorage {
         store.initialize_with_conf_state(conf_state);
         store
     }
+
     pub fn initialize_with_conf_state<T>(&self, conf_state: T)
     where
         ConfState: From<T>,
     {
-        todo!()
+        assert!(!self.initial_state().unwrap().initialized());
+        let mut core = self.wl();
+        core.raft_state.conf_state = ConfState::from(conf_state);
+    }
+
+    pub fn rl(&self) -> RwLockReadGuard<'_, RaftDB> {
+        self.raftdb.read().unwrap()
+    }
+
+    pub fn wl(&self) -> RwLockWriteGuard<'_, RaftDB> {
+        self.raftdb.write().unwrap()
     }
 }
 
 impl Storage for RaftDiskStorage {
     fn initial_state(&self) -> raft::Result<raft::RaftState> {
-        todo!()
+        Ok(self.rl().raft_state.clone())
     }
 
     fn entries(
@@ -95,15 +196,27 @@ impl Storage for RaftDiskStorage {
     }
 
     fn term(&self, idx: u64) -> raft::Result<u64> {
-        todo!()
+        let core = self.rl();
+        if idx == core.snapshot_metadata.index {
+            return Ok(core.snapshot_metadata.term);
+        }
+
+        let offset = core.first_index();
+        if idx < offset {
+            return Err(raft::Error::Store(StorageError::Compacted));
+        }
+        if idx > core.last_index() {
+            return Err(Error::Store(StorageError::Unavailable));
+        }
+        Ok(core.get_entry(idx - offset).term)
     }
 
     fn first_index(&self) -> raft::Result<u64> {
-        todo!()
+        Ok(self.rl().first_index())
     }
 
     fn last_index(&self) -> raft::Result<u64> {
-        todo!()
+        Ok(self.rl().last_index())
     }
 
     fn snapshot(&self, request_index: u64, to: u64) -> raft::Result<raft::prelude::Snapshot> {
